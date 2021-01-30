41b82660af1f6a38b5db6a1d03d579d29e29cd89
pauldubois98 <pauldubois98@gmail.com>
2021-01-30 21:41:23 +0100

    added slides link & embedded video slides show

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ window.onscroll = function() {
     
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         //header.style.flexDirection = "row-reverse"
-        header.style.fontSize = "1.8em";
-        header_space.style.height = "13em";
+        header.style.fontSize = "1.2em";
+        //header_space.style.height = "13em";
         //logo.src = "logo/WestonHouse6_bis_alt.svg"
 
     } else {
         //header.style.flexDirection = "column"
-        header.style.fontSize = "2.5em";
-        header_space.style.height = "13em";
+        header.style.fontSize = "1.8em";
+        //header_space.style.height = "13em";
         //logo.src = "logo/WestonHouse6_bis.svg"
     }
 } 
